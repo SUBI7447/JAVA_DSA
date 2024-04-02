c96022b645dcbae3e9b05e8146881ff9255a3487
SUBI7447 <subodhsubi362@gmail.com>
2024-04-02 15:22:34 +0530

    ADD IN G PART 4

--- 37_GRAPHS/PART_4/Bellman_Ford_Algo.java
+++ 37_GRAPHS/PART_4/Bellman_Ford_Algo.java
@@ -1,10 +1,88 @@
 package PART_4;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Bellman_Ford_Algo {
 
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+
+        }
+
+    }
+
+    static void createGraph(ArrayList<Edge>[] graph) {
+
+        for (int i = 0; i < graph.length; i++) {
+
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+
+        graph[1].add(new Edge(1, 2, -4));
+
+        graph[2].add(new Edge(2, 3, 2));
+
+        graph[3].add(new Edge(3, 4, 4));
+
+        graph[4].add(new Edge(4, 1, -1));
+    }
+
+    public static void B_Algo(ArrayList<Edge> graph[], int src) {
+
+        int dis[] = new int[graph.length];
+
+        for (int i = 0; i < dis.length; i++) {
+
+            if (i != src) {
+
+                dis[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (int j2 = 0; j2 < graph.length-1; j2++) {
+
+            for (int i = 0; i < graph.length; i++) {
+
+                for (int j = 0; j < graph[i].size(); j++) {
+
+                    if (dis[graph[i].get(j).src] != Integer.MAX_VALUE && dis[i] + graph[i].get(j).wt < dis[graph[i].get(j).dest]) {
+
+                        dis[graph[i].get(j).dest] = dis[i] + graph[i].get(j).wt;
+
+                    }
+
+                }
+
+            }
+        }
+
+        for (int i = 0; i < dis.length; i++) {
+
+            System.out.println(dis[i]);
+
+        }
+    }
+
     public static void main(String[] args) {
+
+        int V = 5;
+
+        ArrayList<Edge> graph[] = new ArrayList[V];
+
+        createGraph(graph);
+
+        B_Algo(graph, 0);
 
     }
 
---
+++ 37_GRAPHS/PART_4/Prims_Algo.java
@@ -0,0 +1,110 @@
+package PART_4;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class Prims_Algo {
+
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+
+        }
+
+    }
+
+    static void createGraph(ArrayList<Edge>[] graph) {
+
+        for (int i = 0; i < graph.length; i++) {
+
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 10));
+        graph[0].add(new Edge(0, 2, 15));
+        graph[0].add(new Edge(0, 3, 30));
+
+        graph[1].add(new Edge(1, 0, 10));
+        graph[1].add(new Edge(1, 3, 40));
+
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 3, 50));
+
+        graph[3].add(new Edge(3, 1, 40));
+        graph[3].add(new Edge(3, 2, 50));
+
+    }
+
+    static class Pair implements Comparable<Pair> {
+
+        int v;
+        int cost;
+
+        public Pair(int v, int c) {
+            this.v = v;
+            this.cost = c;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+
+            return this.cost - p2.cost;
+        }
+
+    }
+
+    public static void GET_MST(ArrayList<Edge> graph[]) {
+
+        boolean vis[] = new boolean[graph.length];
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        pq.add(new Pair(0, 0));
+
+        int total_cost = 0;
+
+        while (!pq.isEmpty()) {
+
+            Pair curr = pq.remove();
+
+            if (!vis[curr.v]) {
+
+                vis[curr.v] = true;
+
+                total_cost += curr.cost;
+
+                System.out.println(" TC : " + total_cost);
+
+                for (int i = 0; i < graph[curr.v].size(); i++) {
+
+                    Edge e = graph[curr.v].get(i);
+
+                    pq.add(new Pair(e.dest, e.wt));
+
+                }
+            }
+
+        }
+
+        System.out.println(" TC : " + total_cost);
+
+    }
+
+    public static void main(String[] args) {
+
+        int V = 4;
+
+        ArrayList<Edge> graph[] = new ArrayList[V];
+
+        createGraph(graph);
+
+        GET_MST(graph);
+    }
+
+}
