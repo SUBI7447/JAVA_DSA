74687688c89f8528dcb2c6e721aad19c9194086d
SUBI7447 <subodhsubi362@gmail.com>
2024-04-03 15:56:02 +0530

    FLOOD FILL QUE

---
+++ 37_GRAPHS/PART_5/Flood_Fill.java
@@ -0,0 +1,27 @@
+// Given a m x n integer grid image where represents the pixel value of the image.
+// You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from
+// the pixel imagelsrl[scl.
+// To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting
+// pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with
+// the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+
+public class Flood_Fill {
+
+    public static int[][] Flood_F(int img[][], int sc, int sr, int color, boolean visited[], int original_col) {
+        return img;
+
+    }
+
+    public static void main(String[] args) {
+
+        int img[][] = {
+                { 1, 1, 1 },
+                { 1, 1, 0 },
+                { 1, 0, 1 }
+        };
+
+        boolean[][] bool = new boolean[img.length][img[0].length];
+
+    }
+
+}
